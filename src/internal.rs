f7020ba9ad8f6f9d4a010c42a8a1881942c129e7
Frago9876543210 <al.volf.av@gmail.com>
2020-06-08 14:21:14 +0300

    Rename all methods of the internal module to _hacked

--- src/internal.rs
+++ src/internal.rs
@@ -3,11 +3,11 @@ use std::io::{Read, Result, Write};
 use crate::{CanIo, Endian, Primitive};
 
 pub trait HackedPrimitive: Primitive {
-	fn write<E: Endian, W: Write>(self, w: W) -> Result<()> {
+	fn write_hacked<E: Endian, W: Write>(self, w: W) -> Result<()> {
 		E::write(self, w)
 	}
 
-	fn read<E: Endian, R: Read>(r: R) -> Result<Self> {
+	fn read_hacked<E: Endian, R: Read>(r: R) -> Result<Self> {
 		E::read(r)
 	}
 }
@@ -21,11 +21,11 @@ impl OnlyCanIo for Io {}
 impl<T: Primitive> HackedPrimitive for T {}
 
 pub trait HackedCanIo: CanIo {
-	fn write<E: OnlyCanIo, W: Write>(self, w: W) -> Result<()> {
+	fn write_hacked<E: OnlyCanIo, W: Write>(self, w: W) -> Result<()> {
 		CanIo::write(&self, w)
 	}
 
-	fn read<E: OnlyCanIo, R: Read>(r: R) -> Result<Self> {
+	fn read_hacked<E: OnlyCanIo, R: Read>(r: R) -> Result<Self> {
 		CanIo::read(r)
 	}
 }
