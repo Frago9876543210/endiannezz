853c3f067215bb37eeda1b6697a90eb0748274e1
Frago9876543210 <al.volf.av@gmail.com>
2020-06-11 13:40:31 +0300

    Added inlining features

--- src/internal.rs
+++ src/internal.rs
@@ -3,10 +3,12 @@ use std::io::{Read, Result, Write};
 use crate::{Endian, Io, Primitive};
 
 pub trait HackedPrimitive: Primitive {
+	#[cfg_attr(feature = "inline_primitives", inline)]
 	fn write_hacked<E: Endian, W: Write>(self, w: W) -> Result<()> {
 		E::write(self, w)
 	}
 
+	#[cfg_attr(feature = "inline_primitives", inline)]
 	fn read_hacked<E: Endian, R: Read>(r: R) -> Result<Self> {
 		E::read(r)
 	}
@@ -15,10 +17,12 @@ pub trait HackedPrimitive: Primitive {
 impl<T: Primitive> HackedPrimitive for T {}
 
 pub trait HackedIo: Io {
+	#[cfg_attr(feature = "inline_io", inline(always))]
 	fn write_hacked<E: Endian, W: Write>(&self, w: W) -> Result<()> {
 		Io::write(self, w)
 	}
 
+	#[cfg_attr(feature = "inline_io", inline(always))]
 	fn read_hacked<E: Endian, R: Read>(r: R) -> Result<Self> {
 		Io::read(r)
 	}
