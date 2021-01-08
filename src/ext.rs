5853e1167986782ab75ed78af80dabba46554416
Frago9876543210 <al.volf.av@gmail.com>
2021-01-08 19:29:17 +0300

    Added new ext methods for 0.6.2

--- src/ext.rs
+++ src/ext.rs
@@ -1,12 +1,27 @@
 use std::io::{Read, Result, Write};
 
-use crate::{Endian, Primitive};
+use crate::{BigEndian, Endian, LittleEndian, NativeEndian, Primitive};
 
 /// Allows to write primitive types with differents representation of bytes
 pub trait EndianWriter: Write {
     #[inline]
     fn try_write<E: Endian, T: Primitive>(&mut self, primitive: T) -> Result<()> {
         E::write(primitive, self)
+    }
+
+    #[inline]
+    fn write_ne<T: Primitive>(&mut self, primitive: T) -> Result<()> {
+        self.try_write::<NativeEndian, T>(primitive)
+    }
+
+    #[inline]
+    fn write_le<T: Primitive>(&mut self, primitive: T) -> Result<()> {
+        self.try_write::<LittleEndian, T>(primitive)
+    }
+
+    #[inline]
+    fn write_be<T: Primitive>(&mut self, primitive: T) -> Result<()> {
+        self.try_write::<BigEndian, T>(primitive)
     }
 }
 
@@ -17,6 +32,21 @@ pub trait EndianReader: Read {
     #[inline]
     fn try_read<E: Endian, T: Primitive>(&mut self) -> Result<T> {
         E::read(self)
+    }
+
+    #[inline]
+    fn read_ne<T: Primitive>(&mut self) -> Result<T> {
+        self.try_read::<NativeEndian, T>()
+    }
+
+    #[inline]
+    fn read_le<T: Primitive>(&mut self) -> Result<T> {
+        self.try_read::<LittleEndian, T>()
+    }
+
+    #[inline]
+    fn read_be<T: Primitive>(&mut self) -> Result<T> {
+        self.try_read::<BigEndian, T>()
     }
 }
 
